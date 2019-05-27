7345ed1ee494a20d312d65ed30309f0f1dc28136
yotaiyo <yota071218@gmail.com>
2019-05-27 18:44:26 +0900

    modified comment_box

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -1,4 +1,4 @@
-const CommentBox = React.createClass({
+var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
@@ -10,21 +10,35 @@ const CommentBox = React.createClass({
   }
 });
 
-const CommentList = React.createClass({
+var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
       </div>
     );
   }
 });
 
-const CommentForm = React.createClass({
+var CommentForm = React.createClass({
   render: function() {
     return (
       <div className="commentForm">
         Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
       </div>
     );
   }
