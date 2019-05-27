1d0597d3d0d0a6fcf7a6f6f513561c3ec9b7edbb
yotaiyo <yota071218@gmail.com>
2019-05-27 18:32:34 +0900

    create comment-box

---
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -0,0 +1,31 @@
+const CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+const CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+const CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
